de16b4998883205e342944c86946e8e8a962dedf
1MPERVM <rakhim_impervm@mail.ru>
2021-04-01 00:15:50 +0300

    start

---
+++ forBars/forBars/Calculate.cs
@@ -0,0 +1,51 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forBars
+{
+    class Calculate
+    {
+        static void Main()
+        {
+            Test test = new Test();
+            Volume volume = new Volume();
+            Tariff tariff = new Tariff();
+
+            volume.ServiceId = 1;
+            tariff.ServiceId = 3;
+
+            volume.HouseId = 1;
+            tariff.HouseId = 1;
+
+            volume.Month = DateTime.Now;
+            tariff.PeriodBegin = new DateTime(2020, 11, 21);
+            tariff.PeriodEnd = new DateTime(2021, 06, 24);
+
+            volume.Value = 24;
+            tariff.Value = 32;
+
+
+            test.OnNotify += TestOnNotifyBegin;           
+            test.OnException += Test_OnException;
+            Charge charge = test.CalculateSubsidy(volume, tariff);
+            test.OnNotify += TestOnNotifyBegin;
+            Console.WriteLine($"{charge.Value}");
+           
+        }
+
+        private static void Test_OnException(object sender, Tuple<string, Exception> e)
+        {
+            Console.WriteLine($"Ошибка {e}");
+        }
+
+        private static void TestOnNotifyBegin(object sender, string e)
+        {
+            Console.WriteLine($"Расчет начат-закончен в: {DateTime.Now}");
+        }
+       
+    }
+}
---
+++ forBars/forBars/Test.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using ClassLibrary1;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forBars
+{
+    class Test : ISubsidyCalculation
+    {
+        public event EventHandler<string> OnNotify;
+        public event EventHandler<Tuple<string, Exception>> OnException;
+        public  Charge CalculateSubsidy(Volume volumes, Tariff tariff)
+        {
+            Charge charge = new Charge();
+
+            Test test = new Test();
+            OnNotify?.Invoke(this, $"Расчет начат в: {DateTime.Now}");
+            
+               if (volumes.ServiceId != tariff.ServiceId)
+               {
+                   OnException?.Invoke(this, new Tuple<string, Exception>("ServiseId Error", new Exception("ServiseId не совпадают!")));
+                   throw new Exception("GG");
+               }
+               if (volumes.HouseId != tariff.HouseId)
+               {
+                   OnException?.Invoke(this, new Tuple<string, Exception>("HouseID Error", new Exception("HouseID не совпадают!")));
+                   throw new Exception("GG");
+               }
+               if (volumes.Month > tariff.PeriodEnd || volumes.Month < tariff.PeriodBegin)
+               {
+                   OnException?.Invoke(this, new Tuple<string, Exception>("Period Error", new Exception("Period Error")));
+                   throw new Exception("GG");
+               }
+
+                var value = volumes.Value * tariff.Value;
+                charge.Value = value;
+                charge.Month = volumes.Month;
+                charge.HouseId = volumes.HouseId;
+                charge.ServiceId = volumes.ServiceId;
+
+                OnNotify?.Invoke(this, $"Расчет окончен в: {DateTime.Now}");
+                
+                return charge;  
+
+        }
+        private void OnNotBeg()
+        {
+            Console.WriteLine($"Расчет окончен в: {DateTime.Now}");
+        }
+
+        
+    }
+}
